b8aaaa3bd05a650d0fa787fa00dc20f43eed4736
dity51028 <dipannitamisti10@gmail.com>
2026-04-01 16:56:21 +0530

    relationship added

--- myWebsite/newApp/urls.py
+++ myWebsite/newApp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.app,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
+    path('product/<int:product_id>/',views.product_detail,name='product_detail'),
    
 ]
